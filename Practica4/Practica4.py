f81f6d3d2cb673f37e8bd4dc560ee8a7faa940d0
Adrian-de-Lucas-Gomez <adridelu@ucm.es>
2021-11-19 00:44:06 +0100

    Añadida memoria de la practica4
    Falta por refinarla un poco y ejecutarla para ver la salida por consola.

--- Practica4/Practica4.py
+++ Practica4/Practica4.py
@@ -4,29 +4,6 @@ from scipy.io import loadmat
 import scipy.optimize as minimize
 import displayData as disp
 import checkNNGradients as checkNNG
-import pandas as pd
-
-# def parte1():
-
-#     data= loadmat('ex4data1.mat')
-
-#     y= data['y']
-#     X = data['X']
-
-#     # sample = np.random.choice(X.shape[0], 100)
-#     # disp.displayData(X[sample])
-#     # plt.show()
-
-#     weights = loadmat('ex4weights.mat')
-#     theta1 = weights['theta1']
-#     theta2 = weights['theta2']
-
-#     y_matrix = pd.get_dummies(np.array(y).ravel()).values
-#     A=coste(theta1, theta2, X, y_matrix)
-#     B = costeReg(theta1, theta2, X, y_matrix, 1)
-#     print(B)
-
-#     diff = checkNNG.checkNNGradients(back_propagate, 1)
 
 def parte1():
     datos = loadmat('ex4data1.mat')
@@ -44,40 +21,51 @@ def parte1():
     for i in range(m):
         y_onehot[i][yAux[i]] = 1
 
-    # sample = np.random.choice(X.shape[0], 100)
-    # disp.displayData(X[sample])
-    # plt.show()
+    sample = np.random.choice(X.shape[0], 100)
+    disp.displayData(X[sample])
+    plt.show()
 
     weights = loadmat('ex4weights.mat')
     theta1 = weights['Theta1']
     theta2 = weights['Theta2']
 
-    print("Coste sin regularizar: " + str(coste(X, y_onehot, theta1, theta2))[:5])
-    print("Coste regularizado con lambda=1: " + str(costeReg(X, y_onehot, theta1, theta2, 1))[:5])
-    print()
+    print("Coste sin haber regularizado: ")
+    print(str(coste(X, y_onehot, theta1, theta2))[:5])
+    print("Coste regularizando con lambda valor 1: ")
+    print(str(costeReg(X, y_onehot, theta1, theta2, 1))[:5])
+    print()     #Salto de linea
 
     lambo = 1
-    print("--Comprobación del gradiente--")
+    print("Comprobacion de las variaciones en el gradiente")
     diff = checkNNG.checkNNGradients(redNeuronalPatras, lambo)
-    print("Menor diferencia: " + str(min(diff)))
-    print("Mayor diferencia: " + str(max(diff)))
-    print()
+    print()     #Salto de linea
 
-    #Aprendizaje de los parámetros
-    theta1 = pesosAleatorios(input_size, num_hidden)
-    theta2 = pesosAleatorios(num_hidden, num_labels)
+    print("Menor diferencia: ")
+    print(str(min(diff)))
+    print("Mayor diferencia: ")
+    print(str(max(diff)))
+    print()     #Salto de linea
+
+
+    #Elejimos valores aleatorios para las thetas
+    theta1 = randomWeights(input_size, num_hidden)
+    theta2 = randomWeights(num_hidden, num_labels)
+    #Los guardamos para probar con ellos
     params_rn = np.concatenate((np.ravel(theta1), np.ravel(theta2)))
     
-    #n_iters = 70
-    optimizeResult = minimize.minimize(fun=redNeuronalPatras, x0=params_rn, args=(input_size, num_hidden, num_labels, X, y_onehot, lambo), method='TNC', jac=True, options={'maxiter': 70})
-    evaluacion = comprobar(optimizeResult, input_size, num_hidden, num_labels, X, y)
-    print("Evaluación del entrenamiento de la red: {}%".format(str(evaluacion*100)[:5]))
+    num_Iterations = 70     #Vueltas dadas para tratar de optimizar
+    optimizeResult = minimize.minimize(fun=redNeuronalPatras, x0=params_rn, args=(input_size, num_hidden, num_labels, X, y_onehot, lambo), method='TNC', jac=True, options={'maxiter': num_Iterations})
+    evaluations = precisionChecker(optimizeResult, input_size, num_hidden, num_labels, X, y)
+
+    print("Precision de la red tras ser entrenada:")
+    print(str(evaluations*100)[:5] + "%")
 
 
 def coste(X, Y, theta1, theta2):
 
-	a1, a2, H = redNeuronalPaLante(X, theta1,theta2) #Sacas el resultado
+	a1, a2, H = redNeuronalPaLante(X, theta1,theta2) #Haces la pasada por la red neuronal
 
+    #Queda mas claro dividido por partes
 	op1= -1/(len(X))
 	op2 = Y * np.log(H)
 	op3 = (1-Y) * np.log(1-H)
@@ -153,7 +141,7 @@ def redNeuronalPatras(params_rn, n_input, n_hidden, n_labels, X, y, lambdita):
 
     return coste, gradiente
     
-def comprobar(resOpt, n_input, n_hidden, n_labels, X, y):
+def precisionChecker(resOpt, n_input, n_hidden, n_labels, X, y):
     theta1 = np.reshape(resOpt.x[:n_hidden * (n_input + 1)] , (n_hidden, (n_input+1)))
     theta2 = np.reshape(resOpt.x[n_hidden * (n_input + 1):] , (n_labels, (n_hidden+1)))
 
@@ -161,10 +149,10 @@ def comprobar(resOpt, n_input, n_hidden, n_labels, X, y):
 
     aux = np.argmax(H, axis=1)
     aux += 1
-
+    #calculamos cuantos se han identificado correctamente y lo dividimos por los casos de prueba
     return np.sum(aux == y) / np.shape(H)[0]
 
-def pesosAleatorios(L_ini, L_out):
+def randomWeights(L_ini, L_out):
     E_ini = 0.12
     return np.random.random((L_out, L_ini + 1)) * (2*E_ini) - E_ini
 
