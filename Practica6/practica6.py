7a6023bd73d4a117a14c7611301e92f9428d7878
AndresRuizB <andrui10@ucm.es>
2021-12-02 17:37:17 +0100

    comenzada practica 6

---
+++ Practica6/practica6.py
@@ -0,0 +1,91 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from scipy.io import loadmat
+from scipy.optimize import minimize
+from sklearn.svm import SVC
+from process_email import email2TokenList
+import codecs
+from get_vocab_dict import getVocabDict
+import glob
+import sklearn.model_selection as ms
+# from sklearn.metrics import confusion_matrix
+
+
+
+def visualize_boundary(X, y, svm):
+    x1 = np.linspace(X[:, 0].min(), X[:, 0].max(), 100)
+    x2 = np.linspace(X[:, 1].min(), X[:, 1].max(), 100)
+    x1, x2 = np.meshgrid(x1, x2)
+    yp = svm.predict(np.array([x1.ravel(), x2.ravel()]).T).reshape(x1.shape)
+    pos = (y == 1).ravel()
+    neg = (y == 0).ravel()
+    plt.figure()
+    plt.scatter(X[pos, 0], X[pos, 1], color='black', marker='+')
+    plt.scatter(X[neg, 0], X[neg, 1], color='yellow',
+                edgecolors='black', marker='o')
+    plt.contour(x1, x2, yp)
+    plt.show()
+
+
+def parte1():
+    data = loadmat('ex6data1.mat')
+    X, y = data['X'], data['y'].ravel()
+    #svm = SVC(kernel = 'linear', C=1)
+    svm = SVC(kernel='linear', C=100)
+    svm.fit(X, y.ravel())
+    visualize_boundary(X, y, svm)
+
+
+def parte2():
+    data = loadmat('ex6data2.mat')
+    X, y = data['X'], data['y'].ravel()
+    sigma = 0.1
+    C = 1
+    svm = SVC(kernel='rbf', C=C, gamma=1/(2 * sigma**2))
+    svm.fit(X, y.ravel())
+    visualize_boundary(X, y, svm)
+
+
+def parte3():
+    data = loadmat('ex6data3.mat')
+    X, y = data['X'], data['y'].ravel()
+    Xval, yVal = data['Xval'], data['yval'].ravel()
+    sigmas = [0.01, 0.03, 0.1, 0.3, 1, 3, 10, 30]
+    Cs = [0.01, 0.03, 0.1, 0.3, 1, 3, 10, 30]
+    scores  = np.zeros((len(Cs), len(sigmas)))
+
+    i = 0
+    j = 0
+    for c,i in zip(Cs,range(len(Cs))):
+        for sigma,j in zip(sigmas,range(len(sigmas))):
+            svm = SVC(kernel='rbf', C=c, gamma=1/(2 * sigma**2))
+            svm.fit(X,y.ravel())
+            scores[i,j] = svm.score(Xval, yVal)           
+
+    print("Error minimo ", 1 - scores.max())
+    cOpt = Cs[scores.argmax()//len(sigmas)]
+    sigmaOpt = sigmas[scores.argmax() % len(sigmas)]
+    print("C optimo {}    sigma optimo {}".format(cOpt, sigmaOpt) )
+    svm = SVC(kernel='rbf', C=cOpt, gamma=1/(2 * sigmaOpt**2))
+    svm.fit(X,y.ravel())
+    visualize_boundary(X, y, svm)
+
+def ParteSPAM():
+    corpusDict = getVocabDict()
+
+    allFiles = glob.glob('spam/*.txt')
+    Xspam = np.zeros((len(allFiles), len(corpusDict)))
+    Yspam = np.ones(len(allFiles))
+
+    i = 0
+    for file in allFiles:
+        emails = codecs.open(file, 'r', encoding='utf-8', errors = 'ignore').read()
+        tokens = email2TokenList(emails)
+        palabras = filter(None, [corpusDict.get(x) for x in tokens])
+        for palabra in palabras:
+            Xspam[i, palabra-1] = 1
+        i+=1
+
+
+
+ParteSPAM()
