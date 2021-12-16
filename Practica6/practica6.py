dd4ca1857260a7b5a401bd9b97e33074fce4a768
Adrian-de-Lucas-Gomez <adridelu@ucm.es>
2021-12-16 17:49:59 +0100

    Casi finiquitada la ultima práctica
    Ya lo acabo en casa

--- Practica6/practica6.py
+++ Practica6/practica6.py
@@ -1,3 +1,4 @@
+from re import X
 import numpy as np
 import matplotlib.pyplot as plt
 from scipy.io import loadmat
@@ -8,7 +9,7 @@ import codecs
 from get_vocab_dict import getVocabDict
 import glob
 import sklearn.model_selection as ms
-# from sklearn.metrics import confusion_matrix
+import time
 
 
 
@@ -46,6 +47,55 @@ def parte2():
     visualize_boundary(X, y, svm)
 
 
+def Spam(corpusDict):
+    allFiles = glob.glob('spam/*.txt')
+    Xspam = np.zeros((len(allFiles), len(corpusDict)))
+    Yspam = np.ones(len(allFiles))
+
+    i = 0
+    for file in allFiles:
+        emails = codecs.open(file, 'r', encoding='utf-8', errors = 'ignore').read()
+        tokens = email2TokenList(emails)
+        palabras = filter(None, [corpusDict.get(x) for x in tokens])
+        for palabra in palabras:
+            Xspam[i, palabra-1] = 1
+        i+=1
+
+    Xspam.shape
+    
+def EasyHam(corpusDict):
+    allFiles = glob.glob('easy_ham/*.txt')
+    XeasyHam = np.zeros((len(allFiles), len(corpusDict)))
+    easyHam = np.ones(len(allFiles))
+
+    j = 0
+    for file in allFiles:
+        emails = codecs.open(file, 'r', encoding='utf-8', errors = 'ignore').read()
+        tokens = email2TokenList(emails)
+        palabras = filter(None, [corpusDict.get(x) for x in tokens])
+        for palabra in palabras:
+            XeasyHam[j, palabra-1] = 1
+        j+=1
+    
+    XeasyHam.shape
+
+def HardHam(corpusDict):
+    allFiles = glob.glob('easy_ham/*.txt')
+    XhardHam = np.zeros((len(allFiles), len(corpusDict)))
+    hardHam = np.ones(len(allFiles))
+
+    k = 0
+    for file in allFiles:
+        emails = codecs.open(file, 'r', encoding='utf-8', errors = 'ignore').read()
+        tokens = email2TokenList(emails)
+        palabras = filter(None, [corpusDict.get(x) for x in tokens])
+        for palabra in palabras:
+            XhardHam[k, palabra-1] = 1
+        k+=1
+
+    XhardHam.shape
+
+
 def parte3():
     data = loadmat('ex6data3.mat')
     X, y = data['X'], data['y'].ravel()
@@ -70,22 +120,51 @@ def parte3():
     svm.fit(X,y.ravel())
     visualize_boundary(X, y, svm)
 
-def ParteSPAM():
+ 
+    # Parte 4: Deteccion de SPAM----------------------------------------------------------------------
+
     corpusDict = getVocabDict()
 
-    allFiles = glob.glob('spam/*.txt')
-    Xspam = np.zeros((len(allFiles), len(corpusDict)))
-    Yspam = np.ones(len(allFiles))
+    Spam(corpusDict)
+    EasyHam(corpusDict)
+    HardHam(corpusDict)
 
-    i = 0
-    for file in allFiles:
-        emails = codecs.open(file, 'r', encoding='utf-8', errors = 'ignore').read()
-        tokens = email2TokenList(emails)
-        palabras = filter(None, [corpusDict.get(x) for x in tokens])
-        for palabra in palabras:
-            Xspam[i, palabra-1] = 1
-        i+=1
+    Xtrain, Xtest, Ytrain, Ytest = ms.train_test_split(X, y, test_size=0.2, random_state=1)
+    Xtrain, Xvalid, Ytrain, Yvalid = ms.train_test_split(Xtrain, Ytrain, test_size=0.25, random_state=1)
+    
+    #activamos un contador
+    start= time.process_time()
 
+    sigmas = [0.01, 0.03, 0.1, 0.3, 1, 3, 10, 30]
+    Cs = [0.01, 0.03, 0.1, 0.3, 1, 3, 10, 30]
 
+    correct = np.empty((len(Cs), len(sigmas)))
 
-ParteSPAM()
+    for c,i in zip(Cs,range(len(Cs))):
+        for sigma,j in zip(sigmas,range(len(sigmas))):
+            svm = SVC(kernel='rbf', C=c, gamma=1/(2 * sigma**2))
+            svm.fit(Xtrain,Ytrain)
+            correct[i,j]= svm.score(Xvalid, Yvalid)
+
+    COpt = Cs[correct.argmax()//len(Cs)] 
+    SigmaOpt = sigmas[correct.argmax() % len(sigmas)]
+
+    svm = SVC(kernel='rbf', C=COpt, gamma=1/(2 * SigmaOpt**2))
+    svm.fit(Xtrain, Ytrain)
+
+    #Paramos el contador
+    stop = time.process_time()
+    
+    Score = svm.score(Xtest, Ytest)
+    TimeElapsed = stop - start
+
+    print("\n")
+    print("DETECCION DE SPAWM\n")
+
+    print("Error minimo ", 1 - Score.max())
+    print("C optimo {}    sigma optimo {}".format(COpt, SigmaOpt) )
+    print("Porcentaje aciertos {}%".format(Score*100))
+    print("Tiempo en ejecicion {}segundos".format(TimeElapsed))
+
+parte3()
+
