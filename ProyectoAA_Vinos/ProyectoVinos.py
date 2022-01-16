0b90f1386b3c24788f8014563298d839586b6754
Andres Ruiz <andrui10@ucm.es>
2022-01-16 13:49:17 +0100

    random forest y visualizacion de valores importantes

--- ProyectoAA_Vinos/ProyectoVinos.py
+++ ProyectoAA_Vinos/ProyectoVinos.py
@@ -6,6 +6,8 @@ import seaborn as sns
 
 from sklearn.preprocessing import StandardScaler, LabelEncoder
 from sklearn.svm import SVC
+from sklearn.ensemble import RandomForestClassifier
+
 from sklearn.metrics import classification_report, accuracy_score
 
 import pandas as pd
@@ -26,7 +28,7 @@ def comienzo():
 
     #print(wines.describe())
 
-    ################# SVN ###############################
+    ################# SVM ###############################
 
     #Now seperate the dataset as response variable and feature variabes
     X = wines.drop('quality', axis = 1)
@@ -43,45 +45,76 @@ def comienzo():
     nnormal = counts['normal']
     ngood = counts['good']
 
-    print(f"bad {nbad}, good {ngood}, normal {nnormal}",)
+    print(f"bad {nbad}, good {ngood}, normal {nnormal}")
 
-    sizesTest = { 0.2, 0.3, 0.4}
-    randomStates = range(1,200,10)
+    sizesTest = {0.2, 0.3, 0.4}
 
     maxAccuracy = 0
     bestTestSize = 0
-    bestRandomState = 0
 
     for tSize in sizesTest:
-        for rState in randomStates:
-            X_train, X_test, y_train, y_test = ms.train_test_split(X, y, test_size = tSize, random_state = rState)
-            #Applying Standard scaling to get optimized result
-            sc = StandardScaler()
-            X_train = sc.fit_transform(X_train)
-            X_test = sc.fit_transform(X_test)
+        X_train, X_test, y_train, y_test = ms.train_test_split(X, y, test_size = tSize, random_state = 0)           
 
-            svc = SVC()
-            svc.fit(X_train, y_train)
-            pred_svc = svc.predict(X_test)
-            accuracy = accuracy_score(y_test, pred_svc)
+        sc = StandardScaler()
+        X_train = sc.fit_transform(X_train)
+        X_test = sc.fit_transform(X_test)
 
-            if accuracy> maxAccuracy:
-                maxAccuracy = accuracy
-                bestTestSize = tSize
-                bestRandomState = rState
-            print("testComplete")
+        svc = SVC()
+        svc.fit(X_train, y_train)
+        pred_svc = svc.predict(X_test)
+        accuracy = accuracy_score(y_test, pred_svc)
 
-            
-    print(f"max accuracy {maxAccuracy}, best TestSize {bestTestSize}, best RandomState {bestRandomState}")
-    #print(classification_report(y_test, pred_svc))
+        if accuracy > maxAccuracy:
+            maxAccuracy = accuracy
+            bestTestSize = tSize            
+            bestmodel = pred_svc
+            print(f"new max SVM acc {maxAccuracy}, testSize {bestTestSize}")
+
+
+    print("########### SVM ############")        
+    print(f"max accuracy {maxAccuracy}, best TestSize {bestTestSize}")
+    print(classification_report(y_test, pred_svc))
+    print()
 
     
+    ################# RANDOM FOREST CLASIFICATION ###############################
 
 
+    sizesTest = {0.2, 0.3, 0.4}
 
+    maxAccuracy = 0
+    bestTestSize = 0
 
+    for tSize in sizesTest:
+
+        X_train, X_test, y_train, y_test = ms.train_test_split(X, y, test_size = tSize, random_state = 0)           
+
+        sc = StandardScaler()
+        X_train = sc.fit_transform(X_train)
+        X_test = sc.fit_transform(X_test)
+
+        model2 = RandomForestClassifier(random_state=0)
+        model2.fit(X_train, y_train)
+        y_pred2 = model2.predict(X_test)
+        #print(classification_report(y_test, y_pred2))
+
+        if accuracy> maxAccuracy:
+            maxAccuracy = accuracy
+            bestTestSize = tSize
+            bestModel = y_pred2
+            print(f"new max rForest acc {maxAccuracy}, testSize {bestTestSize}")
+
+    print("########### RANDOM FOREST ###########")
+    print(f"max accuracy {maxAccuracy}, best TestSize {bestTestSize}")
+    print(classification_report(y_test, pred_svc))
 
     ####### COSAS DE ENSEÑAR
+
+    # feat_importances = pd.Series(model2.feature_importances_, index= X.columns)
+    # feat_importances.nlargest(25).plot(kind='barh',figsize=(10,10))
+    # plt.show()
+
+    
     #Evolucion de una de las variables y  relacion con la valoracion del vino
     #fig = plt.figure(figsize = (10,6))
     #sns.barplot(x = 'quality', y = 'fixed acidity', data = wines)
