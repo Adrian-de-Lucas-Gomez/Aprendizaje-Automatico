bc105e818263b8bda6ba565a198a19d00bc7f76c
Andres Ruiz <andrui10@ucm.es>
2022-01-16 14:41:31 +0100

    gradient descent (con libreria)

--- ProyectoAA_Vinos/ProyectoVinos.py
+++ ProyectoAA_Vinos/ProyectoVinos.py
@@ -7,6 +7,7 @@ import seaborn as sns
 from sklearn.preprocessing import StandardScaler, LabelEncoder
 from sklearn.svm import SVC
 from sklearn.ensemble import RandomForestClassifier
+from sklearn.linear_model import SGDClassifier
 
 from sklearn.metrics import classification_report, accuracy_score
 
@@ -28,24 +29,23 @@ def comienzo():
 
     #print(wines.describe())
 
+    
     ################# SVM ###############################
 
     #Now seperate the dataset as response variable and feature variabes
     X = wines.drop('quality', axis = 1)
 
-    #bins = (2, 6.5, 9)
-    bins = (2, 4, 6, 9)
-    group_names = ['bad','normal', 'good']
+    bins = (2, 5.5, 9)
+    group_names = ['bad','good']
     wines['quality'] = pd.cut(wines['quality'], bins = bins, labels = group_names)
     y = wines['quality']
 
     counts = wines['quality'].value_counts()
 
     nbad = counts['bad']
-    nnormal = counts['normal']
     ngood = counts['good']
 
-    print(f"bad {nbad}, good {ngood}, normal {nnormal}")
+    print(f"bad {nbad}, good {ngood}")
 
     sizesTest = {0.2, 0.3, 0.4}
 
@@ -53,7 +53,7 @@ def comienzo():
     bestTestSize = 0
 
     for tSize in sizesTest:
-        X_train, X_test, y_train, y_test = ms.train_test_split(X, y, test_size = tSize, random_state = 0)           
+        X_train, X_test, y_train, y_test = ms.train_test_split(X, y, test_size = tSize, random_state = 0)
 
         sc = StandardScaler()
         X_train = sc.fit_transform(X_train)
@@ -66,19 +66,26 @@ def comienzo():
 
         if accuracy > maxAccuracy:
             maxAccuracy = accuracy
-            bestTestSize = tSize            
-            bestmodel = pred_svc
-            print(f"new max SVM acc {maxAccuracy}, testSize {bestTestSize}")
+            bestTestSize = tSize
 
 
-    print("########### SVM ############")        
+    print("########### SVM ############")
     print(f"max accuracy {maxAccuracy}, best TestSize {bestTestSize}")
+
+    X_train, X_test, y_train, y_test = ms.train_test_split(X, y, test_size = bestTestSize, random_state = 0)
+
+    sc = StandardScaler()
+    X_train = sc.fit_transform(X_train)
+    X_test = sc.fit_transform(X_test)
+
+    svc = SVC()
+    svc.fit(X_train, y_train)
+    pred_svc = svc.predict(X_test)
     print(classification_report(y_test, pred_svc))
     print()
 
     
     ################# RANDOM FOREST CLASIFICATION ###############################
-
 
     sizesTest = {0.2, 0.3, 0.4}
 
@@ -95,24 +102,75 @@ def comienzo():
 
         model2 = RandomForestClassifier(random_state=0)
         model2.fit(X_train, y_train)
-        y_pred2 = model2.predict(X_test)
-        #print(classification_report(y_test, y_pred2))
+        pred_rfc = model2.predict(X_test)
+        accuracy = accuracy_score(y_test, pred_rfc)
 
         if accuracy> maxAccuracy:
             maxAccuracy = accuracy
             bestTestSize = tSize
-            bestModel = y_pred2
-            print(f"new max rForest acc {maxAccuracy}, testSize {bestTestSize}")
 
     print("########### RANDOM FOREST ###########")
     print(f"max accuracy {maxAccuracy}, best TestSize {bestTestSize}")
-    print(classification_report(y_test, pred_svc))
+
+    X_train, X_test, y_train, y_test = ms.train_test_split(X, y, test_size = bestTestSize, random_state = 0)           
+
+    sc = StandardScaler()
+    X_train = sc.fit_transform(X_train)
+    X_test = sc.fit_transform(X_test)
+
+    model2 = RandomForestClassifier(random_state=0)
+    model2.fit(X_train, y_train)
+    pred_rfc = model2.predict(X_test)
+
+    print(classification_report(y_test, pred_rfc))
 
     ####### COSAS DE ENSEÑAR
 
     # feat_importances = pd.Series(model2.feature_importances_, index= X.columns)
     # feat_importances.nlargest(25).plot(kind='barh',figsize=(10,10))
     # plt.show()
+
+
+    ################# GRADIENT DESCENT ###################
+    
+    # Este es el bueno tendremos que probar el de la P2
+
+    sizesTest = {0.2, 0.3, 0.4}
+
+    maxAccuracy = 0
+    bestTestSize = 0
+
+    for tSize in sizesTest:
+
+        X_train, X_test, y_train, y_test = ms.train_test_split(X, y, test_size = tSize, random_state = 0)           
+
+        sc = StandardScaler()
+        X_train = sc.fit_transform(X_train)
+        X_test = sc.fit_transform(X_test)
+
+        sgd = SGDClassifier(penalty=None)
+        sgd.fit(X_train, y_train)
+        pred_sgd = sgd.predict(X_test)
+        accuracy = accuracy_score(y_test, pred_sgd)
+
+        if accuracy > maxAccuracy:
+            maxAccuracy = accuracy
+            bestTestSize = tSize
+
+    print("########### GRADIENT DESCENT ###########")
+    print(f"max accuracy {maxAccuracy}, best TestSize {bestTestSize}")
+    X_train, X_test, y_train, y_test = ms.train_test_split(X, y, test_size = bestTestSize, random_state = 0)           
+
+    sc = StandardScaler()
+    X_train = sc.fit_transform(X_train)
+    X_test = sc.fit_transform(X_test)
+
+    sgd = SGDClassifier(penalty=None)
+    sgd.fit(X_train, y_train)
+    pred_sgd = sgd.predict(X_test)
+
+    print(classification_report(y_test, pred_sgd))
+
 
     
     #Evolucion de una de las variables y  relacion con la valoracion del vino
